203a555db40a18d2a528173464ed436dcbf93da9
Alvaro-Bettoni <alvarobettoni2@gmail.com>
2023-05-05 17:43:24 -0300

    req 2 e 3

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [enable, setEnabled] = useState(true);
+  console.log(email);
+  console.log(password);
   return (
-    <div>Login</div>
+    <div>
+      <input
+        type="email"
+        data-testid="email-input"
+        value={ email }
+        onChange={ (e) => setEmail(e.target.value) }
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        value={ password }
+        maxLength="15"
+        onChange={ (e) => setPassword(e.target.value) }
+      />
+      <button data-testid="login-submit-btn" disabled={ enable }>Enter</button>
+    </div>
   );
 }
