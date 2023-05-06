5f1989af081b3982751237a937fa9b575e510259
thamyris <thamyriscabral@hotmail.com>
2023-05-06 15:56:25 -0300

    fix: lint

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
-import App from '../App';
 import renderWithRouter from './RenderWithRouter/renderWithRouter';
 import Footer from '../components/Footer';
 
