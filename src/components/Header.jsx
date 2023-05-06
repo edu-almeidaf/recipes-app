0e11b3aa4b50fc0929886ca5789ee9a5a35da5a9
Glayson <glaysonbss@gmail.com>
2023-05-06 08:30:25 -0300

    feat: requirements 08 and 09 solved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,49 @@
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, showSearchIcon }) {
+  const [toggleSearchBar, setToggleSearchBar] = useState(false);
+  const history = useHistory();
+
+  const goToProfilePage = () => {
+    history.push('/profile');
+  };
+
+  const toggleSearchBarInput = () => {
+    if (toggleSearchBar) {
+      setToggleSearchBar(false);
+    } else {
+      setToggleSearchBar(true);
+    }
+  };
+
   return (
     <>
-      <img
-        src="/images/profileIcon.svg"
-        alt="profile icon"
-        data-testid="profile-top-btn"
-      />
+      <button onClick={ goToProfilePage }>
+        <img
+          src={ profileIcon }
+          alt="profile icon"
+          data-testid="profile-top-btn"
+        />
+      </button>
+
       {
         showSearchIcon && (
-          <img
-            src="./images/searchIcon.svg"
-            alt="profile icon"
-            data-testid="search-top-btn"
-          />
+          <button onClick={ toggleSearchBarInput }>
+            <img
+              src={ searchIcon }
+              alt="profile icon"
+              data-testid="search-top-btn"
+            />
+          </button>
+        )
+      }
+      {
+        toggleSearchBar && (
+          <input type="search" data-testid="search-input" />
         )
       }
       <h1 data-testid="page-title">{title}</h1>
