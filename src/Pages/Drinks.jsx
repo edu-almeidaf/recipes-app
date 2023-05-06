a59456063a2c560fc8e90d2b6cfd523bad1a4a87
thamyris <thamyriscabral@hotmail.com>
2023-05-06 16:02:30 -0300

    fix: lint

--- src/Pages/Drinks.jsx
+++ src/Pages/Drinks.jsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import Footer from '../components/Footer';
+
 import Header from '../components/Header';
 
 function Drinks() {
   return (
-    <div>
+    <>
       <Header title="Drinks" showSearchIcon />
+
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/Pages/Meals.jsx
+++ src/Pages/Meals.jsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import Footer from '../components/Footer';
+
 import Header from '../components/Header';
 
 function Meals() {
   return (
-    <div>
+    <>
       <Header title="Meals" showSearchIcon />
+
       <Footer />
-    </div>
+    </>
   );
 }
 
