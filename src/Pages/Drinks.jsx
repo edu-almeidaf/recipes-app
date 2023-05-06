d3de96e80ea7b78a84a99dacec03b0507b56a236
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-05-06 18:21:16 -0300

    feat: [Req 19] Ao entrar nas páginas /meals e /drinks, as receitas são renderizadas na tela

--- src/Pages/Drinks.jsx
+++ src/Pages/Drinks.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import { useContext, useEffect } from 'react';
 import Recipes from '../components/Recipes';
 import Header from '../components/Header';
+import { searchBarContext } from '../Context/SearchBarProvider';
 
 export default function Drinks() {
+  const { setApiData } = useContext(searchBarContext);
+  useEffect(() => {
+    const fetchInitialRecipes = async () => {
+      const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+      const MAGIC = 12;
+      const response = await fetch(URL);
+      const data = await response.json();
+      if (data.drinks.length > MAGIC) {
+        const arraySliced = data.drinks.slice(0, MAGIC);
+        setApiData(arraySliced);
+        return;
+      }
+      setApiData(data.drinks);
+    };
+
+    fetchInitialRecipes();
+  }, []);
   return (
     <>
       <Header title="Drinks" showSearchIcon />
--- src/Pages/Meals.jsx
+++ src/Pages/Meals.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import { useContext, useEffect } from 'react';
 import Recipes from '../components/Recipes';
 import Header from '../components/Header';
+import { searchBarContext } from '../Context/SearchBarProvider';
 
 export default function Meals() {
+  const { setApiData } = useContext(searchBarContext);
+  useEffect(() => {
+    const fetchInitialRecipes = async () => {
+      const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+      const MAGIC = 12;
+      const response = await fetch(URL);
+      const data = await response.json();
+      if (data.meals.length > MAGIC) {
+        const arraySliced = data.meals.slice(0, MAGIC);
+        console.log(arraySliced);
+        setApiData(arraySliced);
+        return;
+      }
+      setApiData(data.meals);
+    };
+
+    fetchInitialRecipes();
+  }, []);
   return (
     <>
       <Header title="Meals" showSearchIcon />
